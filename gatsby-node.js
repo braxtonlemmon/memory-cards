77c1b97776262fe9181f69e29b9ed00e9ef80b6b
Braxton Lemmon <braxtonlemmon@gmail.com>
2021-01-29 16:54:31 -0700

    Use sourceNodes API and createNode action to source image data from Picsum to be accessible via GraphQL queries.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,24 @@
  */
 
 // You can delete this file if you're not using it
+const fetch = require('node-fetch');
+
+exports.sourceNodes = async({
+  actions: { createNode },
+  createContentDigest,
+}) => {
+  const result = await fetch('https://picsum.photos/v2/list?page=4')
+  const resultData = await result.json();
+  resultData.forEach(image => {
+    createNode({
+      imageUrl: image.download_url,
+      id: image.id,
+      parent: null,
+      children: [],
+      internal: {
+        type: `picsum`,
+        contentDigest: createContentDigest(image)
+      }
+    })
+  })
+}
