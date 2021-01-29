476e596ed6a7232337c3026d207c8915792f7dee
Braxton Lemmon <braxtonlemmon@gmail.com>
2021-01-29 15:17:18 -0700

    Remove template files.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
+import TestComponent from '../components/TestComponent';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -8,6 +9,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <TestComponent firstName="Braxton" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
